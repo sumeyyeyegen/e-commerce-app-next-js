0f136c2a6d765e329a798d395104e8f234c7423a
Sumeyye Yegen <sumeyyeyegen746@gmail.com>
2023-01-14 02:07:54 +0300

    mockapi url added.(to try)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env:{
+    NEXT_APP_BASE_ENDPOINT:"https://62d53994d4406e523555cb56.mockapi.io",
+    GREETING:"Merhaba dünya"
+  }
 }
 
 module.exports = nextConfig
