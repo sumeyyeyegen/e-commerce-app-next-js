32bcc6921a9a391d7016a9aa0e80cab10935f91f
Sumeyye Yegen <sumeyyeyegen746@gmail.com>
2023-01-14 02:09:25 +0300

    header added to _app page and head updated in _app page.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import store from '../redux/store'
+import { Provider } from 'react-redux';
+import Head from 'next/head';
+import Header from '../components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Otobüs Uygulaması</title>
+      <link href="//netdna.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
+    </Head>
+    <Header />
+    <div className="container">
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </div>
+  </>
 }
